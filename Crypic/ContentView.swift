e098b8bbafee52a38ce02d205bb6f8443272c126
Takeshi Tanaka <takeshi.tanaka@goodpatch.com>
2020-04-23 17:30:27 +0900

    Initial Commit

---
+++ Crypic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypic
+//
+//  Created by emp-mac-takeshitanaka on 2020/04/23.
+//  Copyright © 2020 p0dee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
